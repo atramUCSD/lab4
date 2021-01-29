ba5ada1208ff32a547a03bbe5ee5b5936a31b2c3
atramUCSD <atram@ucsd.edu>
2021-01-28 17:59:25 -0800

    final push

--- routes/project.js
+++ routes/project.js
@@ -1,9 +1,5 @@
-exports.viewProject = function(req,res) {
-    res.render("project");
-};
-
 exports.viewProject = function(req, res){
-    res.render('index', {
+    res.render('project', {
         'projects': [
           { 'name': 'Waiting in Line',
             'image': 'lorempixel.people.1.jpeg',
